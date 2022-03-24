b1466f56806f866b8ff555c5a1c530f76fbd5edd
Matheus <matheus.progra.dev@gmail.com>
2022-03-24 00:03:01 -0300

    Ajustes no andPoint/ categoria/{id}

---
+++ src/main/java/com/nelioalves/cursomc/resoucers/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.resoucers.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandarError> objecNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandarError err = new StandarError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+	}
+
+}
